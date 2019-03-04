958d62fc189eebc9c2d33e6e410029d204c55901
Camilo Arguello Cencosud <camiloandres.arguellorincon@cencosud.com.co>
2019-03-04 06:58:24 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# CA Resume
+
+This is my Resume in a terminal environment. For education purposes.
+
+> Based on @silent-lad project
+
+## Installation
+
+
+```
+
+npm install --save ca-resume
+
+```
+
+## Launch
+
+```
+
+ca-resume
+
+```
+
+Created by @kmiloarguello
---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+"use-strict";
+
+var inquirer = require("inquirer");
+var chalk = require("chalk");
+
+var response = chalk.bold.green;
+
+var resume = require("./resume.json");
+
+var resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: "What do you want to know about me?",
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+function main() {
+  console.log("Hello, my name is CA.");
+  resumeHandler();
+}
+
+function resumeHandler() {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return;
+    }
+    var option = answer.resumeOptions;
+    console.log(response("--------------------------------------"));
+    resume[`${option}`].forEach(info => {
+        if(option == "Tech Stack" || option == "Contact"){
+            console.log(response("|   => " + info));
+        }else{
+            // TODO: render all information
+            var select = info[Object.keys(info)[0]]
+            console.log(response("|   => " + select));
+        }
+    });
+    console.log(response("--------------------------------------"));
+    // console.log(resume[`${option}`]);
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+main();
